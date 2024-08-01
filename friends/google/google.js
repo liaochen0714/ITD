dc7910d960ec24a42c0933e678feb6e269a855b7
liaochen0714 <easonliao0714@gmail.com>
2024-08-01 11:10:03 +0000

    暴雷內容

--- friends/google/google.js
+++ friends/google/google.js
@@ -1,3 +1,11 @@
-function goToPage(page) {
-    window.location.href = page;
+function goToPage(pageUrl) {
+    window.open(pageUrl, '_blank');
+}
+
+function showGoogle() {
+    const googleImg = document.getElementById('google-img');
+    const gayImg = document.querySelector('.gay');
+
+    googleImg.style.display = 'none';
+    gayImg.style.display = 'inline';
 }
